705d25f0f59fc49fa1fcf9d6aa5ad068076a4016
monotykamary <tom81094@gmail.com>
2016-05-12 06:58:25 +0700

    changed nupkg name type in gitkraken

--- scripts/gitkraken.ps1
+++ scripts/gitkraken.ps1
@@ -1,5 +1,5 @@
 param($dir)
 
 # Second extraction
-$output = 7z x $dir\gitkraken-1.2.0-full.nupkg "-o$dir"
-rm $dir\gitkraken-1.2.0-full.nupkg
+$output = 7z x $dir\gitkraken-1.3.0-full.nupkg "-o$dir"
+rm $dir\gitkraken-1.3.0-full.nupkg
